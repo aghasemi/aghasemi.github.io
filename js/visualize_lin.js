dd1a0277f0718003873ac6a061091a75ac373f89
aghasemi <ghasemi.a.ir@gmail.com>
2015-03-10 15:27:31 +0100

    link to work removed from home

--- js/visualize_lin.js
+++ js/visualize_lin.js
@@ -12,20 +12,22 @@
   // Sets the fill attribute of the circle to white
   //circle.attr("fill", "#a0ee90");
 
-
-
+  var camLocs=new Array(nc);
+  var camArrayGeom=new Array();
   var locStart=centr-bl/2;
   var locEnd=centr+bl/2;
 
-  //At least one cam?
+  //At least one camera?
   if (nc>=1)
   {
-    draw_vertical_camera(paper,locStart,ns,fv,isdithered)
+    	draw_vertical_camera(paper,locStart,ns,fv,isdithered)
+	camLocs[0]=locStart;
   }
   //At least two?
   if (nc>=2)
   {
     draw_vertical_camera(paper,locEnd,ns,fv,isdithered)
+		camLocs[nc-1]=locEnd;
   }
   //The rest in between
   var spacing=bl/(nc-1);
@@ -40,7 +42,9 @@
 
       var loc=locStart+i*spacing;
     }
-    draw_vertical_camera(paper,loc,ns,fv,isdithered)
+    var rlocs=draw_vertical_camera(paper,loc,ns,fv,isdithered);
+	camArrayGeom.push(rlocs);
+		camLocs[i]=loc;
 
 
   }
@@ -51,7 +55,8 @@
   var zlow=(bl/2)/(tan_a);
   var zradius=ns*(0.5*bl)*(1/(2*tan_a))-zlow;
   draw_sector(paper,paper.width/2,paper.height-zlow,zradius,90-fv/2,90+fv/2,{"stroke":"gray","stroke-width":3});
-
+	camArrayGeom.push(camLocs);
+	return camArrayGeom;
 
 }
 
@@ -59,73 +64,74 @@
 //Draws inly pinhole cameras
 function draw_vertical_camera(paper,loc,ns,fv,isdithered)
 {
-  var colors = ['red', 'green', 'blue', 'magenta', 'orange'];
+	  var colors = ['red', 'green', 'blue', 'magenta', 'orange'];
 
-  var a=Math.PI*fv/360;
-  var tan_a=Math.tan(a);
-  //var x=rad/tan_a;
-  //var f=Math.sqrt(r*r+x*x)-r;
-  var zmax=paper.height/100;
-
-  var sensorStart=loc-paper.height*tan_a;
-  var sensorEnd=loc+paper.height*tan_a;
-
-
+	  var a=Math.PI*fv/360;
+	  var tan_a=Math.tan(a);
+	  //var x=rad/tan_a;
+	  //var f=Math.sqrt(r*r+x*x)-r;
+	  var zmax=paper.height/100;
+	  var rayLocs=new Array();
+	  var sensorStart=loc-paper.height*tan_a;
+	  var sensorEnd=loc+paper.height*tan_a;
 
 
-  {
 
 
-    //Drawing the triangle for NOP
-    var line1 = paper.path( ["M", loc, paper.height, "L", sensorStart, 0] );
-    var line2 = paper.path( ["M", loc, paper.height, "L", sensorEnd, 0] );
-    var line3 = paper.path( ["M",sensorStart, 0, "L", sensorEnd, 0] );
-
-    line1.attr({"stroke":colors[0],"stroke-width":1});
-    line2.attr({"stroke":colors[1],"stroke-width":1});
+	  {
 
 
-  }
+		 //Drawing the triangle for NOP
+		 var line1 = paper.path( ["M", loc, paper.height, "L", sensorStart, 0] );
+		 var line2 = paper.path( ["M", loc, paper.height, "L", sensorEnd, 0] );
+		 var line3 = paper.path( ["M",sensorStart, 0, "L", sensorEnd, 0] );
 
-  var q=(sensorEnd-sensorStart)/ns;
-
-  var elements = paper.set();
-
-
-  //var tri   = paper.path( ["M", centr+rad+f, centr, "L", centr+rad, centr+h1 , "L", centr+rad, centr-h1] );
-  //tri.attr({"stroke-width": 0, fill: "yellow"});
-  var holder=sensorStart;
-  //var holder2=centr-h2;
-
-  var col=2;
-  while (sensorEnd-holder>q)
-  {
-    if (isdithered)
-    {
-      var rnd_jump=Math.random()*q;
-    }
-    else
-    {
-      var rnd_jump=q;
-    }
-    holder=holder+rnd_jump;
+		 line1.attr({"stroke":colors[0],"stroke-width":1});
+		 line2.attr({"stroke":colors[1],"stroke-width":1});
 
 
-    {
-      var line = paper.path( ["M", loc, paper.height, "L", holder,0 ] );
-    }
+	  }
 
-    line.attr({"stroke":colors[(col++)%colors.length],"stroke-width":1});
-    elements.push(line);
+	  var q=(sensorEnd-sensorStart)/ns;
 
-  }
+	  var elements = paper.set();
 
 
-  elements.push(line1);
-  elements.push(line2);
-  elements.push(line3);
+	  //var tri   = paper.path( ["M", centr+rad+f, centr, "L", centr+rad, centr+h1 , "L", centr+rad, centr-h1] );
+	  //tri.attr({"stroke-width": 0, fill: "yellow"});
+	  var holder=sensorStart;
+	  //var holder2=centr-h2;
+	  rayLocs.push(holder);
+	  var col=2;
+	  while (sensorEnd-holder>q)
+	  {
+		 if (isdithered)
+		 {
+		   var rnd_jump=Math.random()*q;
+		 }
+		 else
+		 {
+		   var rnd_jump=q;
+		 }
+		 holder=holder+rnd_jump;
+		 rayLocs.push(holder);
 
-  //elements.rotate(an,centr,centr) ; // deprecated
+		 {
+		   var line = paper.path( ["M", loc, paper.height, "L", holder,0 ] );
+		 }
+
+		 line.attr({"stroke":colors[(col++)%colors.length],"stroke-width":1});
+		 elements.push(line);
+
+	  }
+
+
+	  elements.push(line1);
+	  elements.push(line2);
+	  elements.push(line3);
+	  rayLocs.push(sensorEnd);
+	  return rayLocs;
+	  //elements.rotate(an,centr,centr) ; // deprecated
 }
 
 
@@ -159,8 +165,8 @@ function draw_vertical_camera(paper,loc,ns,fv,isdithered)
     paper.canvas.style.backgroundColor = '#f1f09f';
 
     //draw_vertical_camera(paper,paper.width/3,ns,fv,isdithered)
-    draw_linear_array(paper,ns,nc,bl,fv,isdithered,isrand)
-
+    var geom=draw_linear_array(paper,ns,nc,bl,fv,isdithered,isrand)
+	console.log(geom);
 
 }
 
