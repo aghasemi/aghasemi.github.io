668cd9d477227c60e1a5e5efe96af5b6b10978c1
alireza <ghasemi.a.ir@gmail.com>
2021-10-20 12:34:59 +0200

    Load style dynamically from config file

--- js/index.js
+++ js/index.js
@@ -1,35 +1,50 @@
 
-renderMD = async () => {
+const loadCSS = (url, callback) => {
+    var el = document.createElement("link");
+    el.href = url;
+    el.rel = "stylesheet";
+    el.type = "text/css";
+    document.head.appendChild(el);
+    if(typeof(callback) == "function") {
+        el.onload = callback(el);
+    }
+    return el;
+}
+
+const renderFromAnchor = async () => {
 
 	const hash = window.location.hash
 	const pgHash = hash.length===0 ? 'Home':  hash.substring(1)
 
 	const config = await (await fetch(`/config.json`)).json()
-	const my_name = config['title']
+	const siteName = config['title']
 	const pages = config['pages']
 	
-	document.getElementById('my-name').innerHTML = my_name
-	document.getElementById('copyright-holder').innerHTML = my_name
+	document.getElementById('my-name').innerHTML = siteName
+	document.getElementById('copyright-holder').innerHTML = siteName
 	document.getElementById('pages').innerHTML =''
 
-	let current_page_path = `/content/${pgHash}.md` //Default path. Will be used if an array is given as list
-	const page_titles = Array.isArray(pages) ? pages : Object.keys(pages)  
+	let currentPagePath = `/content/${pgHash}.md` //Default path. Will be used if an array is given as list
+	const pageTitles = Array.isArray(pages) ? pages : Object.keys(pages)  
 	
-	page_titles.forEach(page_title => {
-		const page_anchor = page_title.replace(/ /g, '_'); //Remove all whitespaces
+	pageTitles.forEach(page_title => {
+		const pageAnchor = page_title.replace(/ /g, '_'); //Remove all whitespaces
 
-		current_page_path = (page_anchor===pgHash && (typeof pages[page_title] !== 'undefined') ) ? pages[page_title] : current_page_path
-		document.getElementById('pages').innerHTML += `<li class="nav-item"><a class="nav-link" href="#${page_anchor}">${page_title}</a></li>`
+		currentPagePath = (pageAnchor===pgHash && (typeof pages[page_title] !== 'undefined') ) ? pages[page_title] : currentPagePath
+		document.getElementById('pages').innerHTML += `<li class="nav-item"><a class="nav-link" href="#${pageAnchor}">${page_title}</a></li>`
 	});
 
-	
-	document.title =  `${my_name} - ${pgHash}`;
-	const result   = await (await fetch(current_page_path) ).text()
+
+	const styleFile = 'styleFile' in config ? config['styleFile'] : '/css/style.css'
+	loadCSS(styleFile)
+
+	document.title =  `${siteName} - ${pgHash}`;
+	const result   = await (await fetch(currentPagePath) ).text()
 	document.getElementById('content').innerHTML = marked(result);
 	
 	
 }
 
-window.addEventListener('hashchange', () => renderMD())
+window.addEventListener('hashchange', () => renderFromAnchor())
 
-window.onload = () => renderMD()
+window.onload = () => renderFromAnchor()
